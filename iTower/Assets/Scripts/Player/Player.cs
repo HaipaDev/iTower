a3c2041b0aa076217f71c3e2e30ab6cfb15de199
HyperGamesDev <hypergamesdev@gmail.com>
2021-05-24 21:48:09 +0200

    Slippery ground, bouncing from walls

--- iTower/Assets/Scripts/Player/Player.cs
+++ iTower/Assets/Scripts/Player/Player.cs
@@ -4,39 +4,52 @@ using UnityEngine;
 
 public class Player : MonoBehaviour{
     public static Player instance;
-    [Header("Variables")]
-    public float speed=4;
+    [Header("Properties")]
+    public float speed=6;
     public float jumpForce=4;
     public float defaultGravity=2;
+    public float groundFriction=0.075f;
     [SerializeField] Transform feetPos;
-    [SerializeField] float checkRadius=0.3f;
+    [SerializeField] float checkRadius=0.15f;
     [SerializeField] LayerMask whatIsGround;
-    [Header("Other")]
+    [Header("Variables")]
     [HideInInspector]public bool damaged;
     [HideInInspector]public bool healed;
     [SerializeField]bool isGrounded;
     [SerializeField]float jumpTimer;
     public float jumpTime;
     [SerializeField]bool isJumping;
-    public Animator animator;
-
+    public int wallBounces;
+    Animator anim;
     Rigidbody2D rb;
     float moveInput;
+    int faceDir=-1;
+    float wallBounceTimer;
+
     void Awake(){instance=this;}
     void Start(){
         rb=GetComponent<Rigidbody2D>();
+        anim=GetComponent<Animator>();
     }
     void Update(){
-        if(moveInput>0){GetComponent<SpriteRenderer>().flipX=true;}else if(moveInput<0){GetComponent<SpriteRenderer>().flipX=false;}//Flip the sprite
+        if(rb.velocity.x>0){GetComponent<SpriteRenderer>().flipX=true;faceDir=1;}else if(rb.velocity.x<0){GetComponent<SpriteRenderer>().flipX=false;faceDir=-1;}//Flip the sprite
         MovePlayerJump();
+        if(moveInput==0){//Slide
+            if(rb.velocity.x>0.5f||rb.velocity.x<-0.5f){
+                var t=1;if(rb.velocity.x<0){t=-1;}
+                rb.velocity=new Vector2(rb.velocity.x-(groundFriction*t),rb.velocity.y);
+            }else{if(wallBounces==0){rb.velocity=new Vector2(0,rb.velocity.y);}}
+        }
+        wallBounceTimer-=Time.deltaTime;
+        if(wallBounceTimer<=0)wallBounces=0;
     }
     void FixedUpdate(){
         MovePlayerHorizontal();
     }
     void MovePlayerHorizontal(){
         moveInput=Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("Speed", Mathf.Abs(moveInput));
-        rb.velocity=new Vector2(moveInput*speed,rb.velocity.y);
+        anim.SetFloat("Speed", Mathf.Abs(moveInput));
+        if(moveInput!=0&&wallBounces==0)rb.velocity=new Vector2(moveInput*speed,rb.velocity.y);
     }
     void MovePlayerJump(){
         isGrounded=Physics2D.OverlapCircle(feetPos.position,checkRadius,whatIsGround);//Check if grounded
@@ -57,15 +70,46 @@ public class Player : MonoBehaviour{
         if(!isGrounded&&!isJumping){jumpTimer=0;}
     }
 
-    void OnTriggerStay(Collider other){
-        if(other.gameObject.tag=="Ground"){
-            GetComponent<Collider>().material.dynamicFriction=0;
+    void BounceWall(){
+        Debug.Log("Bounced");
+        wallBounces++;
+        rb.velocity=new Vector2((speed*faceDir)*-1,rb.velocity.y);
+    }
+
+    
+    /*void OnTriggerEnter2D(Collider2D other){
+        if(other.gameObject.tag.Contains("Wall")){
+            if(wallBounces<3)BounceWall();
         }
     }
- 
-    /*void OnTriggerExit(Collider other){
-        if(other.gameObject.tag=="Ground"){
-            GetComponent<Collider>().material.dynamicFriction=1;
+    void OnTriggerStay2D(Collider2D other){
+        if(other.gameObject.tag.Contains("Wall")){
+            //if(wallBounces<3)BounceWall();
+        }
+    }
+    void OnTriggerExit2D(Collider2D other){
+        if(other.gameObject.tag.Contains("Wall")){
+            wallBounces=0;
         }
     }*/
+
+    void OnCollisionEnter2D(Collision2D other){
+        if(other.gameObject.tag.Contains("Wall")){
+            if(wallBounces<3)BounceWall();
+        }
+    }
+    void OnCollisionStay2D(Collision2D other){
+        if(other.gameObject.tag.Contains("Platform")){
+            
+        }
+        if(other.gameObject.tag.Contains("Wall")){
+            if(wallBounces<3)BounceWall();
+        }
+    }
+    void OnCollisionExit2D(Collision2D other){
+        if(other.gameObject.tag.Contains("Wall")){
+            wallBounceTimer=0.1f;
+            //wallBounces=0;
+        }
+    }
 }
